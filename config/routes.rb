154edf1ffc7e0ae5abd7f59fd602262866cc859d
vika <vichechka.uvarova@gmail.com>
2021-10-24 11:44:33 +0300

    comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  # get 'static_pages/home' #3 runs, 6 assertions, 0 failures, 0 errors, 0 skips
   get 'static_pages/help'
   get 'static_pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,6 +8,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
+  #worked 2 runs, 4 assertions, 0 failures, 0 errors, 0 skips
   test "should get help" do
     get static_pages_help_url
     #get :help
