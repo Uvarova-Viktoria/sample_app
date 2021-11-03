6418af2889ecbac3d4832ae94531f567e276b762
vika <vichechka.uvarova@gmail.com>
2021-11-03 22:50:36 +0300

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,16 +13,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     
   end
  
-  # test "valid signup information" do
-  #   get signup_path
-  #   assert_difference 'User.count', 1 do
-  #     post users_path, params: { user: { name:  "Example User",
-  #                                           email: "user@example.com",
-  #                                           password:              "password",
-  #                                           password_confirmation: "password" } }
-     
-  #   end
-  #   assert_template 'users/show'
-  #   assert is_logged_in?
-  # end
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                            email: "user@example.com",
+                                            password:              "password",
+                                            password_confirmation: "password" } }
+    follow_redirect!
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
 end
